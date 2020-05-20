ec4d32bb2ec5e9cd20d0872e3eeb4dbea4fd9e8f
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-05-20 23:15:08 +0530

    check the config

--- server/config/config.js
+++ server/config/config.js
@@ -5,8 +5,7 @@ const config = {
 	App_name: process.env.APP_NAME || 'Aj Hookhu',
 	port: process.env.PORT || 4001,
 	root_path: path.resolve(__dirname),
-	GOOGLE_KEY:
-		'AAAAhSql7lE:APA91bETmhShzQ-_pgQIoajEzd5XHLKZrQv4uD1_QckZuYRiY0nlCUlX-kPMZnh9EtgFB8GJEg_HLcWsXbT0E3JzcrFg979XUEhrDieysYE7Sl43Y-ipfq2fUdN-7wwMo6zDKbqU5dne'
+	GOOGLE_KEY: process.env.GOOGLE_KEY,
 };
 
 module.exports = config;
